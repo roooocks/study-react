d19a12305a14ad7a7b35cef32a66d83644de8814
roooocks <dspdaw@gmail.com>
2025-01-14 18:44:43 +0900

    reducer 셋팅

--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import router from './router';
+import rootReducer from './modules';
+
+const store = configureStore({
+  reducer: rootReducer,
+  // 나중에 미들웨어 들어가면 쓸 예정
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware().concat(logger, thunk, sagaMiddleware),
+  devTools: composeWithDevTools(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 );
 
